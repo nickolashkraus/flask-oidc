368e01d753d77a5ff5ba0d52a27ffd16642d261f
nickolashkraus <0x@nickolaskraus.io>
2023-01-18 09:52:52 -0600

    Clean up and update documentation

--- scripts/decode.py
+++ scripts/decode.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Decode JWT and output its header, payload, and signature.
+Decode a JWT and output its header, payload, and signature.
 """
 import sys
 
@@ -17,38 +17,51 @@ def decode(token_string: str) -> None:
     """
     parts = token_string.split(".")
     header, payload, signature = parts[0], parts[1], parts[2]
-    # Fix error:
+    for x in [header, payload]:
+        try:
+            # NOTE: The header and payload of the JWT are base64url encoded.
+            print(base64.urlsafe_b64decode(pad(x)))
+        except Exception as ex:
+            print(
+                f"An error occurred decoding the following string: {signature}\nError: {ex}\n")
+    # NOTE: The signature of the JWT is NOT base64 encoded!
     #
-    #   binascii.Error: Incorrect padding
+    #   HMACSHA256(
+    #     base64UrlEncode(header) + "." +
+    #     base64UrlEncode(payload),
+    #     secret)
     #
-    # base64.b64decode will truncate any extra padding, provided there is
-    # enough in the first place. Simply add the maximum number of padding
-    # characters that you would ever need, which is two (b'==') and base64
-    # will truncate any unnecessary ones.
-    #
-    # Fix error:
-    #
-    #   binascii.Error: Invalid base64-encoded string: number of data
-    #   characters (333) cannot be 1 more than a multiple of 4
-    #
-    # Pad bytes such that length is a multiple of 4.
-    print(base64.b64decode(pad(header)))
-    print(base64.b64decode(pad(payload)))
-    try:
-      # TODO: binascii.Error: Invalid base64-encoded string: number of data
-      # characters (329) cannot be 1 more than a multiple of 4
-      print(base64.b64decode(pad(signature)))
-    except Exception as ex:
-        print(
-            f"An error occurred decoding the following string: {signature}\nError: {ex}\n")
+    # See: https://jwt.io/introduction
+    print(signature)
 
 def pad(s: str) -> str:
     """
-    Pad base64-encoded string such that length is a multiple of 4.
+    Pad a base64-encoded string such that its length is a multiple of 4.
+
+    Fixes error:
+
+      binascii.Error: Incorrect padding
+
+    base64.b64decode will truncate any extra padding, provided there is
+    enough in the first place. Simply add the maximum number of padding
+    characters that you would ever need, which is two (b'==') and base64
+    will truncate any unnecessary ones.
+
+    Fixes error:
+
+      binascii.Error: Invalid base64-encoded string: number of data
+      characters (xxx) cannot be x more than a multiple of 4
+
+    :type s: str
+    :param s: base64-encoded string
+
+    :rtype: str
+    :return: padded base64-encoded string
     """
     x = len(s) % 4
     if x > 0:
-        return s + '=' * (4 - x) + '===='
+        # Pad string such that its length is a multiple of 4.
+        return s + '=' * (4 - x)
     else:
         return s
 
